e2f582b7573fcaca95d8f7a94189a952ef756ea7
kamen.dimitrov1 <kamen.dimitrov@flaxandteal.co.uk>
2023-05-10 10:51:30 +0300

    WIP: testing concourse directories

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -3,8 +3,17 @@
 cwd=$(pwd)
 
 export GOPATH=$cwd/go
-
+echo $(pwd)
 pushd dp-search-api
   make build && mv build/$(go env GOOS)-$(go env GOARCH)/* $cwd/build
   cp Dockerfile.concourse $cwd/build
+  echo $cwd
+  ls $cwd
+  echo $cwd/build
+  ls $cwd/build
 popd
+
+echo $(pwd)
+ls $(pwd)
+echo $(pwd)/build
+ls $(pwd)
