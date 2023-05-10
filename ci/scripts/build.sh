c6010ef697d4a0886cc72b5d067f23646df81910
kamen.dimitrov1 <kamen.dimitrov@flaxandteal.co.uk>
2023-05-10 12:01:40 +0300

    WIP: testing out CD

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -3,17 +3,8 @@
 cwd=$(pwd)
 
 export GOPATH=$cwd/go
-echo $(pwd)
-pushd dp-search-api
-  make build && mv build/$(go env GOOS)-$(go env GOARCH)/* $cwd/build
-  cp Dockerfile.concourse $cwd/build
-  echo $cwd
-  ls $cwd
-  echo $cwd/build
-  ls $cwd/build
-popd
 
-echo $(pwd)
-ls $(pwd)
-echo $(pwd)/build
-ls $(pwd)/build
+pushd dp-search-api
+  make build && mv build/$(go env GOOS)-$(go env GOARCH)/* ../$cwd/build
+  cp Dockerfile.concourse ../$cwd/build
+popd
